a80351b2cb50399623b0c14055076ec7af12783f
juny524 <juny5241@gmail.com>
2024-07-25 00:12:20 +0900

    I changed the motor driver. Now it should perform as a radio-controlled tank. The tank runs smoothly.

--- tank.py
+++ tank.py
@@ -1,67 +1,39 @@
 import bluetooth
 import struct
 from machine import Pin
-import machine
 import time
 
-led = Pin(15, Pin.OUT)
 # L298N Pins
-IN1 = machine.Pin(2, machine.Pin.OUT)
-IN2 = machine.Pin(3, machine.Pin.OUT)
-ENA = machine.Pin(4, machine.Pin.OUT)
+IN1 = Pin(0, Pin.OUT)
+IN2 = Pin(1, Pin.OUT)
 
 # モーター2のピン定義
-IN3 = machine.Pin(5, machine.Pin.OUT)
-IN4 = machine.Pin(6, machine.Pin.OUT)
-ENA2 = machine.Pin(7, machine.Pin.OUT)
+IN3 = Pin(3, Pin.OUT)
+IN4 = Pin(4, Pin.OUT)
 
-MOTOR_SPEED = 52428 # マックスの80%くらいの速度、これに固定
+STBY = Pin(2, Pin.OUT)
+# スタンバイモード解除
+STBY.value(1)
 
-# PWM設定
-ENA_PWM = machine.PWM(ENA)
-ENA_PWM.freq(1000)  # 周波数1kHz
 
-ENA2_PWM = machine.PWM(ENA2)
-ENA2_PWM.freq(1000)  # 周波数1kHz
 
-def set_motor_speed_right(speed):
-    if 0 <= speed <= 1023:
-        ENA_PWM.duty_u16(speed * 64)  # 0-1023 を 0-65535 にスケールアップ
-    else:
-        print("Speed must be between 0 and 1023")
 
-def set_motor_speed_left(speed):
-    if 0 <= speed <= 1023:
-        ENA2_PWM.duty_u16(speed * 64)  # 0-1023 を 0-65535 にスケールアップ
-    else:
-        print("Speed must be between 0 and 1023")
-
-def move_motor(direction, speed=818):
+def move_motor(direction):
     if direction == 'forward':
-        IN1.high()
-        IN2.low()
-        set_motor_speed_right(speed)
+        IN1.value(1);
         print("forward")
     elif direction == 'backward':
-        IN1.low()
-        IN2.high()
-        set_motor_speed_right(speed)
+        IN2.value(1);
         print("backward")
     elif direction == 'forward2':
-        IN3.high()
-        IN4.low()
-        set_motor_speed_left(speed)
-        print("backward")
+        IN3.value(1);
+        print("forward")
     elif direction == 'backward2':
-        IN3.low()
-        IN4.high()
-        set_motor_speed_left(speed)
+        IN4.value(1);
         print("backward")
     else:
         print("Invalid direction. Use 'forward' or 'backward'.")
         return
-    
-    
     
 
 
@@ -80,8 +52,10 @@ def ledtest(received_value):
         move_motor('backward2')
 
     time.sleep(0.2)
-    set_motor_speed_right(0)
-    set_motor_speed_left(0)
+    IN1.value(0);
+    IN2.value(0);
+    IN3.value(0);
+    IN4.value(0);
     #set_motor_speed(32768)  # Set speed to 50%
     #set_motor_speed(MOTOR_SPEED)  # Set speed to 50%
     # Run motor for 5 seconds
